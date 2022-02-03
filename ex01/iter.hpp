dbb03f08ed5c34063698c204971995362cfe7d2e
Crysicia <7459685+Crysicia@users.noreply.github.com>
2022-02-03 12:11:30 +0100

    Fix ex01 for correction

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,9 +1,16 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template<typename A, typename L>
-void iter(A* array, const L& length, void (*f)(A&)) {
-	for (L i = 0; i < length; i++) {
+template<typename A, typename size_t>
+void iter(A* array, const size_t& length, void (*f)(A&)) {
+	for (size_t i = 0; i < length; i++) {
+		f(array[i]);
+	}
+}
+
+template<typename A, typename size_t>
+void iter(A* array, const size_t& length, void (*f)(const A&)) {
+	for (size_t i = 0; i < length; i++) {
 		f(array[i]);
 	}
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -11,12 +11,23 @@ void ft_putnbr(int& i) {
 	std::cout << i << std::endl;
 }
 
+void ft_putnbr_const(const int& i) {
+	std::cout << i << std::endl;
+}
+
 int main(void) {
 	int int_array[LENGTH] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	const int int_array_const[LENGTH] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	char char_array[LENGTH] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
 
 	std::cout << "---- Call iter on int array ----" << std::endl;
 	iter(int_array, LENGTH, ft_putnbr);
+
+	std::cout << "---- Call iter on const int array ----" << std::endl;
+	iter(int_array_const, LENGTH, ft_putnbr_const);
+
+	std::cout << "---- Call iter on const int array ----" << std::endl;
+	iter(int_array, LENGTH, ft_putnbr_const);
 
 	std::cout << std::endl << "---- Call iter on char array ----" << std::endl;
 	iter(char_array, LENGTH, ft_putchar);
